72ef469d5b51df169336de5e85c78ee354147dc0
Gaurav Sharma <gauravsharma04550@gmail.com>
2026-07-05 22:56:01 +0530

    fix all user controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,7 +18,7 @@ const registerUser = asyncHandler(async(req, res) => {
 
 
     const {fullName, email, username, password} = req.body
-    console.log("email", email);
+    // console.log("email", email);
     
     // validation approach 1 --> multiple if statments
     // if(fullName == "") {
@@ -34,16 +34,22 @@ const registerUser = asyncHandler(async(req, res) => {
         throw new ApiError(400, "All fields are required");
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username}, {email}]
     })
     
     if(existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
-
+    //console.log(req.files);
+    
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
@@ -61,6 +67,7 @@ const registerUser = asyncHandler(async(req, res) => {
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
         email,
+        password,
         username: username.toLowerCase(),
     })
 
